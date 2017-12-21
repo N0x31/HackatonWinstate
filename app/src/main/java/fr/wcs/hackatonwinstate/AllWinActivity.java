93523cedda73e46664a12b5052d1ddc8066512e8
quent176 <quentin7611@yahoo.fr>
2017-12-21 15:58:50 +0100

    l

---
+++ app/src/main/java/fr/wcs/hackatonwinstate/AllWinActivity.java
@@ -0,0 +1,16 @@
+package fr.wcs.hackatonwinstate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllWinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_win);
+
+
+
+    }
+}
