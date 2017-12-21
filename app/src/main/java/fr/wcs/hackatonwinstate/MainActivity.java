b0dcc9c5f5df2119bc1a72c3594aa5f0125c95eb
quent176 <quentin7611@yahoo.fr>
2017-12-21 14:46:46 +0100

    main

--- app/src/main/java/fr/wcs/hackatonwinstate/MainActivity.java
+++ app/src/main/java/fr/wcs/hackatonwinstate/MainActivity.java
@@ -1,8 +1,11 @@
 package fr.wcs.hackatonwinstate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonTakePicture = (Button) findViewById(R.id.takepicturebutton);
+
+        buttonTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CountActivity.class));
+            }
+        });
 
     }
 }
