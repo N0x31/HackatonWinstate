40606a9afadfb62195b5ffd7a5eeae452ac2f3e2
Nono <lombardo.nora@gmail.com>
2017-12-21 15:57:03 +0100

    design home page

--- app/src/main/java/fr/wcs/hackatonwinstate/MainActivity.java
+++ app/src/main/java/fr/wcs/hackatonwinstate/MainActivity.java
@@ -2,9 +2,15 @@ package fr.wcs.hackatonwinstate;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+
+import org.rajawali3d.surface.IRajawaliSurface;
+import org.rajawali3d.surface.RajawaliSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
