7402dc7533216627ef35440f21e8d5ce66c40a88
Nono <lombardo.nora@gmail.com>
2017-12-22 14:49:31 +0100

    ajout nouvelles pages

---
+++ app/src/main/java/fr/wcs/hackatonwinstate/VirusActivity.java
@@ -0,0 +1,13 @@
+package fr.wcs.hackatonwinstate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VirusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_virus);
+    }
+}
